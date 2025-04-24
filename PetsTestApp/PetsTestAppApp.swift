db14d8f1b2c571a96d674c7c550b0d55f66c2b36
Nazar <famina1998@gmail.com>
2025-04-24 22:47:11 +0300

    Initial Commit

---
+++ PetsTestApp/PetsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsTestAppApp.swift
+//  PetsTestApp
+//
+//  Created by Nazar on 24/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PetsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
